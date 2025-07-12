91e04f239e2c8bbc696f9942f7ca00187163a6f1
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2025-07-12 12:38:18 +0600

    Add dynamic path segments

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the index page.")
+
+
+def monthly_challenges(request, month):
+    response_text = None
+    if month == "january":
+        response_text = "Hello, world. You're at the January challenge."
+    elif month == "february":
+        response_text = "Hello, world. You're at the February challenge."
+    else:
+        HttpResponseNotFound("Not found")
+    return HttpResponse(response_text)
