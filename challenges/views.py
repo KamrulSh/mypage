121f58d2a6a911a2048ecb4ee8bc901613ea4f96
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2025-07-12 19:22:40 +0600

    Variable interpolation in template

--- challenges/views.py
+++ challenges/views.py
@@ -41,6 +41,8 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         response_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "challenge_name": response_text, "month_name": month.capitalize()
+        })
     except KeyError:
         return HttpResponseNotFound("<h1>Not found</h1>")
