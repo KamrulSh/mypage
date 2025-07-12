6aa5eaf08cedc4b2e7bb036536c1954bec73139d
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2025-07-12 12:47:01 +0600

    Add path converters

--- challenges/views.py
+++ challenges/views.py
@@ -5,12 +5,18 @@ def index(request):
     return HttpResponse("Hello, world. You're at the index page.")
 
 
+def monthly_challenges_by_number(request, month):
+    return HttpResponse(
+        f"Hello, world. You're at the monthly challenge {month}."
+    )
+
+
 def monthly_challenges(request, month):
     response_text = None
     if month == "january":
-        response_text = "Hello, world. You're at the January challenge."
+        response_text = f"Hello, world. You're at the {month.upper()} challenge."
     elif month == "february":
-        response_text = "Hello, world. You're at the February challenge."
+        response_text = f"Hello, world. You're at the {month.upper()} challenge."
     else:
         HttpResponseNotFound("Not found")
     return HttpResponse(response_text)
