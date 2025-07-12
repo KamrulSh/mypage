7d7105c2e38cf6dd850d8184ff03f52f7ec70f41
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2025-07-12 13:12:26 +0600

    Add more dynamic paths

--- challenges/views.py
+++ challenges/views.py
@@ -1,22 +1,36 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "January challenge",
+    "february": "February challenge",
+    "march": "March challenge",
+    "april": "April challenge",
+    "may": "May challenge",
+    "june": "June challenge",
+    "july": "July challenge",
+    "august": "August challenge",
+    "september": "September challenge",
+    "october": "October challenge",
+    "november": "November challenge",
+    "december": "December challenge",
+}
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index page.")
 
 
-def monthly_challenges_by_number(request, month):
-    return HttpResponse(
-        f"Hello, world. You're at the monthly challenge {month}."
-    )
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Not found")
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect(f"/challenges/{redirect_month}")
 
 
-def monthly_challenges(request, month):
-    response_text = None
-    if month == "january":
-        response_text = f"Hello, world. You're at the {month.upper()} challenge."
-    elif month == "february":
-        response_text = f"Hello, world. You're at the {month.upper()} challenge."
-    else:
-        HttpResponseNotFound("Not found")
-    return HttpResponse(response_text)
+def monthly_challenge(request, month):
+    try:
+        response_text = monthly_challenges[month]
+        return HttpResponse(response_text)
+    except KeyError:
+        return HttpResponseNotFound("Not found")
